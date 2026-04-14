a6bc6bc0cd86a3fd3c19212f1294ca1d9e8e1737
countryBI <nextcountry78@>
2026-04-14 11:46:53 +0300

    remove build artifacts from repo

--- public/presenters_files/app_page_tsx_25567594._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_090abb2c._.js",
-  "static/chunks/d70c2_lucide-react_dist_esm_icons_cd0a8770._.js"
-],
-    source: "dynamic"
-});
